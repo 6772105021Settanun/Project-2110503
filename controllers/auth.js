3f0e4ff677b7abbdae11b22c72f07ef1b676ab3c
6772105021Settanun <6772105021@student.chula.ac.th>
2025-04-24 14:31:11 +0700

    Final Project

--- controllers/auth.js
+++ controllers/auth.js
@@ -35,6 +35,8 @@ exports.register = async (req,res,next)=>{
 //@route     POST /api/v1/auth/login
 //@access    Public
 exports.login=async (req,res,next)=>{
+    try{
+
     const {email, password}=req.body;
 
     //Validate email & password
@@ -60,28 +62,37 @@ exports.login=async (req,res,next)=>{
     //const token=user.getSignedJwtToken();
     //res.status(200).json({success:true,token});
     sendTokenResponse(user,200,res);
+    
+    }catch(err){
+        return res.status(401).json({success:false, msg: 'Cannot convert email or password to string'});
+    }
 };
 
-//Get token from model, create cookie and send response
-const sendTokenResponse=(user, statusCode, res)=>{
-
-    //Create token
-    const token=user.getSignedJwtToken();
-
+// Get token from model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
+    // Create token
+    const token = user.getSignedJwtToken();
     const options = {
-        expires:new Date(Date.now()+process.env.JWT_COOKIE_EXPIRE*24*60*60*1000),
-        httpOnly: true
+      expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+      httpOnly: true,
     };
-
-    if(process.env.NODE_ENV==='production'){
-        options.secure=true;
+  
+    if (process.env.NODE_ENV === 'production') {
+      options.secure = true;
     }
-
-    res.status(statusCode).cookie('token',token,options).json({
+  
+    res.status(statusCode)
+      // .cookie('token', token, options)
+      .json({
         success: true,
-        token
-    });
-}
+        // Add for frontend
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        // End for frontend
+        token,
+      });
+  };
 
 //@desc      Get current Logged in user
 //@route     POST /api/v1/auth/me
@@ -93,4 +104,12 @@ exports.getMe=async(req,res,next)=>{
         success:true,
         data:user
     });
+};
+
+//@desc     Log user out / clear cookie
+//@route    GET /api/v1/auth/logout
+//@access   Private
+exports.logout=async(req,res,next)=>{
+    res.cookie('token','none',{expires:newDate(Date.now()+ 10*1000), httpOnly:true});
+    res.status(200).json({success:true, data:{}});
 };
--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
 const connectDB = require('./config/db');
+//const mongoSanitize=require('express-mongo-sanitize');
+//const helmet=require('helmet');
+//const {xss}=require('express-xss-sanitizer');
+//const rateLimit=require('express-rate-limit');
+//const hpp=require('hpp');
+const cors=require('cors');
 
 //Load env vars
 dotenv.config({path:'./config/config.env'});
@@ -14,13 +20,36 @@ const hospitals =require('./routes/hospitals');
 const auth = require('./routes/auth');
 const appointments = require('./routes/appointments');
 
+
 const app=express();
 
 //Body parser
 app.use(express.json());
-
+app.use(cors());
 //Cookie parser
 app.use(cookieParser());
+
+//Sanitize data
+//app.use(mongoSanitize());
+
+//Set security headers
+//app.use(helmet());
+
+//Prevent XSS attacks
+//app.use(xss());
+
+//Rate Limiting
+//const limiter =rateLimit({
+    //windowsMs:10*60*1000,//10 mins
+    //max:100
+//});
+//app.use(limiter);
+
+//Prevent http param pollutions 
+//app.use(hpp());
+
+//Enable CORS
+//app.use(cors());
 
 //Mount routers
 app.use('/api/v1/hospitals',hospitals);
